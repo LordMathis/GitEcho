83e5b4c483fdc25818e10696b9b41af0121e047f
LordMathis <matus@namesny.com>
2023-05-25 23:04:20 +0200

    Make repo config members public

--- pkg/common/repo.go
+++ pkg/common/repo.go
@@ -3,8 +3,8 @@ package common
 import "github.com/go-git/go-git/v5"
 
 type RepositoryBackupConfig struct {
-	src_repo      *git.Repository
-	pull_interval int
-	s3bucket      string
-	local_path    string
+	SrcRepo      *git.Repository
+	PullInterval int
+	S3bucket     string
+	LocalPath    string
 }

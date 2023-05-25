32bf5a9d8fe5a82410125970701359b8900e86ea
LordMathis <matus@namesny.com>
2023-05-25 23:04:55 +0200

    Walk local repo path when uploading to s3

--- pkg/storage/s3.go
+++ pkg/storage/s3.go
@@ -2,6 +2,11 @@ package storage
 
 import (
 	"context"
+	"fmt"
+	"io/fs"
+	"log"
+	"os"
+	"path/filepath"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
@@ -30,9 +35,34 @@ func newS3Client(bucket string) (*s3Client, error) {
 
 func (s *s3Client) Push(repo *common.RepositoryBackupConfig) error {
 	uploader := manager.NewUploader(s.client)
-	result, err := uploader.Upload(context.TODO(), &s3.PutObjectInput{
-		Bucket: aws.String("my-bucket"),
-		Key:    aws.String("my-object-key"),
-		Body:   uploadFile,
-	})
+
+	err := filepath.WalkDir(repo.LocalPath,
+		func(path string, d fs.DirEntry, err error) error {
+			if err != nil {
+				return err
+			}
+
+			if !d.IsDir() {
+				return nil
+			}
+
+			uploadFile, err := os.Open(path)
+			if err != nil {
+				return err
+			}
+
+			result, err := uploader.Upload(context.TODO(), &s3.PutObjectInput{
+				Bucket: aws.String("my-bucket"),
+				Key:    aws.String("my-object-key"),
+				Body:   uploadFile,
+			})
+
+			fmt.Println(result)
+
+			return nil
+		})
+
+	if err != nil {
+		log.Println(err)
+	}
 }

da64c499eb13c7dbb00df9b928add4050c099ebd
LordMathis <matus@namesny.com>
2023-06-19 21:55:11 +0200

    Create main

--- cmd/gitecho/gitecho.go
+++ cmd/gitecho/gitecho.go
@@ -1,1 +1,19 @@
 package main
+
+import (
+	"log"
+
+	"github.com/LordMathis/GitEcho/pkg/db"
+	"github.com/LordMathis/GitEcho/pkg/http"
+)
+
+func main() {
+	var err error
+	db.DB, err = db.ConnectDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.DB.CloseDB()
+
+	http.Start()
+}

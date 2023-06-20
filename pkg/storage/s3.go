df619e69b59ad53bdebb4f035dac0cc371f60e82
LordMathis <matus@namesny.com>
2023-06-20 21:51:03 +0200

    Rework s3 storage client

--- pkg/storage/s3.go
+++ pkg/storage/s3.go
@@ -1,66 +1,81 @@
 package storage
 
 import (
-	"context"
 	"fmt"
-	"io/fs"
 	"os"
 	"path/filepath"
+	"strings"
+	"sync"
 
-	"github.com/aws/aws-sdk-go-v2/aws"
-	"github.com/aws/aws-sdk-go-v2/config"
-	"github.com/aws/aws-sdk-go-v2/feature/s3/manager"
-	"github.com/aws/aws-sdk-go-v2/service/s3"
-
-	"github.com/LordMathis/GitEcho/pkg/common"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
 )
 
-type S3Client struct {
-	client *s3.Client
-	bucket string
-}
+var (
+	once    sync.Once
+	sess    *session.Session
+	sessErr error
+)
 
-func NewS3Client(bucket string) (*S3Client, error) {
-	cfg, err := config.LoadDefaultConfig(context.TODO())
-	if err != nil {
-		return nil, err
-	}
-	client := s3.NewFromConfig(cfg)
-	return &S3Client{
-		client: client,
-		bucket: bucket,
-	}, nil
-}
-
-func (s *S3Client) Push(backup_repo *common.BackupRepo) error {
-	uploader := manager.NewUploader(s.client)
-
-	err := filepath.WalkDir(backup_repo.LocalPath,
-		func(path string, d fs.DirEntry, err error) error {
-			if err != nil {
-				return err
-			}
-
-			if !d.IsDir() {
-				return nil
-			}
-
-			uploadFile, err := os.Open(path)
-			if err != nil {
-				return err
-			}
-			defer uploadFile.Close()
-
-			result, err := uploader.Upload(context.TODO(), &s3.PutObjectInput{
-				Bucket: aws.String(backup_repo.S3Bucket),
-				Key:    aws.String(path),
-				Body:   uploadFile,
-			})
-
-			fmt.Println(result)
-
-			return err
+func getSession() (*session.Session, error) {
+	once.Do(func() {
+		sess, sessErr = session.NewSession(&aws.Config{
+			Region: aws.String("your-region"), // Replace with your AWS region
 		})
+	})
+	return sess, sessErr
+}
+
+// UploadDirectory uploads the files in the specified directory (including subdirectories) to an S3 storage bucket.
+// If a file already exists in the remote storage, it will be overwritten.
+func UploadDirectory(bucketName, directoryPath string) error {
+	// Get the session
+	sess, err := getSession()
+	if err != nil {
+		return err
+	}
+
+	// Create a new S3 service client
+	svc := s3.New(sess)
+
+	// WalkDir through the directory recursively
+	err = filepath.WalkDir(directoryPath, func(path string, d os.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+
+		// Skip directories
+		if d.IsDir() {
+			return nil
+		}
+
+		// Open the file
+		f, err := os.Open(path)
+		if err != nil {
+			return err
+		}
+		defer f.Close()
+
+		// Prepare the S3 object key
+		key := filepath.Join("your-prefix", strings.TrimPrefix(path, directoryPath)) // Replace "your-prefix" with your desired S3 key prefix
+
+		// Create the input parameters for the S3 PutObject operation
+		input := &s3.PutObjectInput{
+			Bucket: aws.String(bucketName),
+			Key:    aws.String(key),
+			Body:   f,
+		}
+
+		// Perform the S3 PutObject operation
+		_, err = svc.PutObject(input)
+		if err != nil {
+			return err
+		}
+
+		fmt.Printf("Uploaded file %s\n", key)
+		return nil
+	})
 
 	return err
 }

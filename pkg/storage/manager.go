d23d0b09b68b840a20b5a3e82ddb196f3f154365
LordMathis <matus@namesny.com>
2023-07-23 12:27:53 +0200

    Create backup repo and storage managers

--- pkg/storage/manager.go
+++ pkg/storage/manager.go
@@ -29,6 +29,18 @@ func (sm *StorageManager) GetStorage(name string) Storage {
 	return sm.storages[name]
 }
 
+func (sm *StorageManager) GetAllStorages() []Storage {
+	sm.mutex.RLock()
+	defer sm.mutex.RUnlock()
+
+	storages := make([]Storage, 0, len(sm.storages))
+	for _, stor := range sm.storages {
+		storages = append(storages, stor)
+	}
+
+	return storages
+}
+
 func (sm *StorageManager) DeleteStorage(name string) {
 	sm.mutex.Lock()
 	defer sm.mutex.Unlock()

1d80a7cdb9cd97ba15dea4fbb260099e91b7f997
LordMathis <matus@namesny.com>
2023-09-19 22:54:26 +0200

    Add generate key cli option

--- cmd/main.go
+++ cmd/main.go
@@ -2,19 +2,34 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"log"
 	"os"
 	"os/signal"
 	"syscall"
 
 	"github.com/LordMathis/GitEcho/pkg/backup"
 	"github.com/LordMathis/GitEcho/pkg/config"
+	"github.com/LordMathis/GitEcho/pkg/encryption"
 	"github.com/LordMathis/GitEcho/pkg/storage"
 )
 
 func main() {
 
 	configPath := flag.String("f", "config.yaml", "Path to the config file")
+	generateKey := flag.Bool("g", false, "Generate encryption key and exit")
+
 	flag.Parse()
+
+	if *generateKey {
+		key, err := encryption.GenerateEncryptionKey()
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		fmt.Println("Generated encryption key:", string(key))
+		return
+	}
 
 	config, err := config.ReadConfig(*configPath)
 	if err != nil {
